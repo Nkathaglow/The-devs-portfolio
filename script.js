8bf01dc8fa0400ef68a68b008cc10fd4abe5c6f5
Nkatha_Glory <kinyua6945@gmail.com>
2023-03-11 10:54:15 +0300

    Completed project

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This would've been serial killer content";
+h2.textContent = "This is my portfolio";
 
 document.querySelector("body").appendChild(h2);
 
